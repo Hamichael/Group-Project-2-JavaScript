32e78194a2c98a98061617433214d46f16331fd4
Hamichael <40220929+Hamichael@users.noreply.github.com>
2018-10-07 19:11:05 -0400

    Implementation of set() by Grethel
    set() implementation [Grethel]. Modifications made to make it 1 function instead of 3 [Michael].
    Minor fixes to countUp/countDown/stop. Added comments.
    Implementation of set button being disabled [Michael].

---
+++ contributions.md
@@ -0,0 +1,15 @@
+<b>Michael Ha</b>					<br/>
+Java Script implementation:			<br/>
+countUp()							<br/>
+countDown()							<br/>
+stop()								<br/>
+									<br/>
+Imported:							<br/>
+convertSecs(str)					<br/>
+									<br/>
+<b>Susana</b>						<br/>
+HTML and CSS						<br/>
+									<br/>
+<b>Grethel</b>						<br/>
+Java Script implementation:			<br/>
+set()								<br/>
--- project2/project2.js
+++ project2/project2.js
@@ -2,54 +2,100 @@ let clock1;
 let clock2;
 let clock3;
 
-function countUp(duration, display, clock) {
-	clearInterval(this.clock);
-	display.value = "00:00";
-    let timer = duration, minutes, seconds;
-    this.clock = setInterval(function () {
-		timer++;
-		
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-		
-		display.value = minutes + ":" + seconds;
-		
-		//console.log(minutes + ":" + seconds);
-		//console.log(timer);
-		
-    }, 1000);
+//Display is textbox.
+function set(display) {
+	let seconds = "00";
+	let minutes = prompt("Please set time between 1 to 30 minutes: ");
+	if (minutes === null) {return;}//Returns on Cancel
+	while (minutes > 30 || minutes < 1) {
+		minutes = prompt("Invalid quantity. Please select a time between 1 and 30 minutes: ");
+		if (minutes === null) {return;}//Returns on Cancel
+	}
+	
+	// Formatting
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	
+	display.value = minutes + ":" +seconds;
 }
 
-function countDown(duration, display, clock) {
+//Duration is timer listed.
+//Display is textbox.
+//setButton refers to the button to be disabled.
+//Clock using this.clock refers to the current clock variable in use.
+function countUp(duration, display, setButton, clock) {
+	setButton.disabled = true; 
 	clearInterval(this.clock);
-    let timer = duration, minutes, seconds;
+	
+	//Get duration, use it as a stop point
+	length = duration;
+	console.log(length);
+	
+	let timer = 0, minutes, seconds;
+	display.value = "00:00";
+    
     this.clock = setInterval(function () {
-		timer--;
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+		timer++;
+		// Converting Time
+		minutes = parseInt(timer / 60, 10)
+		seconds = parseInt(timer % 60, 10);
+		
+		// Formatting
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
 		
 		display.value = minutes + ":" + seconds;
 		
 		//console.log(minutes + ":" + seconds);
 		//console.log(timer);
 		
-		if (timer == 0) {
-			clearInterval(this.clock);
+		//Stop timer after the number set
+		if (timer >= length) {
+			stop(setButton, clock);
 		}
 		
     }, 1000);
 }
 
-function stop(clock) {
+//Duration is timer listed.
+//Display is textbox.
+//setButton refers to the button to be disabled.
+//Clock using this.clock refers to the current clock variable in use.
+function countDown(duration, display, setButton, clock) {
+	setButton.disabled = true; 
+	clearInterval(this.clock);
+	
+    let timer = duration, minutes, seconds;
+    this.clock = setInterval(function () {
+		timer--;
+		// Converting Time
+		minutes = parseInt(timer / 60, 10)
+		seconds = parseInt(timer % 60, 10);
+
+		// Formatting
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+		
+		display.value = minutes + ":" + seconds;
+		
+		//console.log(minutes + ":" + seconds);
+		//console.log(timer);
+		
+		//Stop timer when it hits 0
+		if (timer <= 0) {
+			stop(setButton, clock);
+		}
+		
+    }, 1000);
+}
+
+//setButton refers to the button to be enabled.
+//Clock using this.clock refers to the clock variable to be disabled.
+function stop(setButton, clock) {
+	setButton.disabled = false; 
 	clearInterval(this.clock);
 }
 
+//Function to convert text clock format into seconds
 function convertSecs(str) {
     var p = str.split(':'),
         s = 0, m = 1;
